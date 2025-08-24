8dae4a3d4d0393c7f223e83e62ae409323836bd3
つづこ <>
2025-08-24 16:45:36 +0900

    ダミーデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Event.destroy_all
+Attendance.destroy_all
+
+e1 = Event.create!(
+  title: "Railsもくもく会",
+  starts_at: 2.days.from_now.change(hour: 19, min: 0),
+  ends_at: 2.days.from_now.change(hour: 21, min: 0),
+  location: "オンライン",
+  capacity: 10,
+  description: "初心者歓迎！進捗ゆるっと共有。"
+)
+
+e2 = Event.create!(
+  title: "週末ハッカソン",
+  starts_at: 5.days.from_now.change(hour: 10, min: 0),
+  ends_at: 5.days.from_now.change(hour: 18, min: 0),
+  location: "渋谷コワーキング",
+  description: "小さく作って発表まで！"
+)
+
+e1.attendances.create!(name: "user1")
+e1.attendances.create!(name: "user2")
